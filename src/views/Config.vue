1f7fb083e01d4ae3846c61d75e9526e3a33a1c5e
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2018-06-20 08:54:20 +0200

    Input area for config

--- src/views/Config.vue
+++ src/views/Config.vue
@@ -1,14 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is the config page</h1>
+  <div >
+    <el-card class="box-card">
+  <div slot="header" class="clearfix">
+    <span>Config</span>
+    <el-button style="float: right; padding: 3px 0" type="text">Validate</el-button>
+  </div>
+     <el-input
+  type="textarea"
+  :rows="20"
+  placeholder="Please input"
+  v-model="textarea">
+</el-input>
+</el-card>
   </div>
 </template>
 <script lang="ts">
-
 export default {
   data() {
-    return {}
+    return {
+      config:{}
+    };
   },
   methods: {}
-}
+};
 </script>
