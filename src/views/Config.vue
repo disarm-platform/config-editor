eb08859268d7970e8e67d6fcfc9cca75d379da86
Nicolai Davies <Nicolai.d@hotmail.com>
2018-06-20 12:22:17 +0200

    formatting

--- src/views/Config.vue
+++ src/views/Config.vue
@@ -1,17 +1,17 @@
 <template>
   <div >
     <el-card class="box-card">
-  <div slot="header" class="clearfix">
-    <span>Config</span>
-    <el-button style="float: right; padding: 3px 0" type="text" @click="validate_config">Validate</el-button>
-  </div>
-     <el-input
-  type="textarea"
-  :rows="20"
-  placeholder="Please input"
-  v-model="config">
-</el-input>
-</el-card>
+      <div slot="header" class="clearfix">
+        <span>Config</span>
+        <el-button style="float: right; padding: 3px 0" type="text" @click="validate_config">Validate</el-button>
+      </div>
+      <el-input
+        type="textarea"
+        :rows="20"
+        placeholder="Please input"
+        v-model="config">
+      </el-input>
+    </el-card>
   </div>
 </template>
 <script lang="ts">
@@ -29,8 +29,8 @@ export default {
       this.popup(JSON.stringify(validation_result));
     },      
     popup(message:string) {
-        this.$message(message);
-      }
+      this.$message(message);
+    }
   }
 };
 </script>
