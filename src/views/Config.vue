73109dcbc35d909cca5ef9c788938f8decfe7e37
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2018-06-20 11:04:32 +0200

    Validates

--- src/views/Config.vue
+++ src/views/Config.vue
@@ -3,24 +3,30 @@
     <el-card class="box-card">
   <div slot="header" class="clearfix">
     <span>Config</span>
-    <el-button style="float: right; padding: 3px 0" type="text">Validate</el-button>
+    <el-button style="float: right; padding: 3px 0" type="text" @click="validate_config">Validate</el-button>
   </div>
      <el-input
   type="textarea"
   :rows="20"
   placeholder="Please input"
-  v-model="textarea">
+  v-model="config">
 </el-input>
 </el-card>
   </div>
 </template>
 <script lang="ts">
+import {validate} from '@locational/config-validation'
 export default {
   data() {
     return {
-      config:{}
+      config:""
     };
   },
-  methods: {}
+  methods: {
+    validate_config(){
+      let validation_result = validate(JSON.parse(this.config))
+      console.log(validation_result);
+    }
+  }
 };
 </script>
