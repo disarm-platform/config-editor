328f925a2e2017b5790204aa5e49655c3772c584
Jonathan Smith <jonathan@peoplesized.com>
2018-06-22 14:36:34 +0200

    stub components

---
+++ src/views/config_components/applets.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>bunch of applets</div>
+</template>
+
+<script>
+  //import { mapState, mapActions, mapMutations } from 'vuex'
+
+  export default {
+    name: 'applets',
+    mounted() {
+    },
+    data() {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/config_components/decorators.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+  export default {
+    name: 'validations'
+  }
+</script>
+
+<style scoped>
+
+</style>
