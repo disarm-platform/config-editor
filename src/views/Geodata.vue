5641dbe5f9c5e668700f74b7ee3e05d6de526f21
Nicolai Davies <Nicolai.d@hotmail.com>
2018-06-22 11:06:31 +0200

    Can add new layer

--- src/views/Geodata.vue
+++ src/views/Geodata.vue
@@ -44,15 +44,16 @@
       </el-table-column>
     </el-table>
 
-    <!-- TODO: Fix this, auto uploads to action props -->
-    <el-upload style="margin-top: 2em;" @on-success="on_upload_file" action="#">
-      <el-button slot="trigger" size="small" type="primary">select file</el-button>
-      <div class="el-upload__tip" slot="tip">jpg/png files with a size less than 500kb</div>
-    </el-upload>
+    <div style="margin-top: 2em;">
 
-    <el-row style="margin-top: 2em;">
-      <el-button disabled>Save</el-button>
-    </el-row>
+      <el-input style="margin: 1em 0;" placeholder="Layer name" v-model="new_layer_name"></el-input>
+      <!-- TODO: Fix this, auto uploads to action props -->
+      <el-upload ref="upload" :limit="1" :auto-upload="false" :on-change="on_upload_file" action="#">
+        <el-button slot="trigger" size="small" type="primary">select file</el-button>
+        <el-button style="margin-left: 10px;" size="small" type="success" @click="save_new_layer">Save layer</el-button>
+        <!-- <div class="el-upload__tip" slot="tip">jpg/png files with a size less than 500kb</div> -->
+      </el-upload>
+    </div>
   </el-card>
   </div>
 </template>
@@ -60,12 +61,24 @@
 import Vue from 'vue'
 import {validate_layer_schema, summarise} from '@locational/geodata-support'
 
+function upload_file_as_text(file) {
+  return new Promise((resolve) => {
+    const reader = new FileReader()
+    reader.onloadend = () => {
+      resolve(reader.result)
+    }
+    reader.readAsText(file)
+  })
+}
+
 export default Vue.extend({
   props:{
     geodata_layers: Array
   },
   data() {
     return {
+      new_layer_name: '',
+      file: null,
       alert: {
         message: '',
         type: '' // warning or success
@@ -86,8 +99,23 @@ export default Vue.extend({
       // TODO: Emit event, instead of setting parent data
       this.$set(this.geodata_layers, index, {...this.geodata_layers[index], validation_status})
     },
-    on_upload_file(response, file, fileList) {
-      console.log(response, file, fileList)
+    async save_new_layer() {
+      const geojson = await upload_file_as_text(this.file.raw)
+
+      const new_layer = {
+        name: this.new_layer_name,
+        file_name: this.file.name,
+        geojson: JSON.parse(geojson),
+        validation_status: '',
+        field_summary: []
+      }
+
+      this.geodata_layers.push(new_layer)
+
+      this.$refs.upload.clearFiles();
+    },
+    on_upload_file(file, fileList) {
+      this.file = file
     },
     // ui
     tableRowClassName({row, rowIndex}) {
