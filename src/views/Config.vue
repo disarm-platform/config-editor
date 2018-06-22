d6088e25aecd1f593a8fdf9165abfc0b4acae5fa
Nicolai Davies <Nicolai.d@hotmail.com>
2018-06-22 10:30:10 +0200

    Moving in the right direction, not quite working

--- src/views/Config.vue
+++ src/views/Config.vue
@@ -50,19 +50,68 @@ export default Vue.extend({
     validate_config(){
       // create geodata in correct format
       const geodata = {}
+      const geodata_summary =  {}
       for (const layer of this.geodata_layers) {
         geodata[layer.name] = layer.geojson
+        geodata_summary[layer.name] = layer.field_summary
       }
-      
 
-      const location_selection = generate_location_selection(this.config.spatial_hierarchy, geodata)
+
+      const spatial_hierarchy = {
+        ...this.config.spatial_hierarchy,
+        geodata_summary
+      }
+      debugger
+      
+      const location_selection_result = generate_location_selection(spatial_hierarchy, geodata)
+      const location_selection = location_selection_result.location_selection
+
       const config = {
         ...this.config,
         location_selection
       }
-      let validation_result = validate(this.config)
-      this.$emit('config_validation', true);
-      this.validation_result = JSON.stringify(validation_result);
+
+      const a = validate(config)
+      const validation_result = this.determine_validation_result(a)
+
+      if (validation_result.passed) {
+        this.$emit('config_validation', true);
+        this.validation_result = "Configuration passed all validations"
+      } else {
+        this.validation_result = 'Schema validation failed ' + JSON.stringify(validation_result.support_messages)
+        this.$emit('config_validation', false);
+      }
+    },
+    determine_validation_result(response) {
+      if (response.status.startsWith('Red')){
+        return {
+          passed: false,
+          support_messages: response.support_messages
+        }
+      }
+
+      const edge_statuses_that_fail = response.edge_messages.filter(e => e.status.startsWith('Red'))
+
+      const messages_to_show = edge_statuses_that_fail.map(e => {
+        if (e.custom_edge_responses.length) {
+          const custom_message = e.custom_edge_responses
+            .filter(cer => cer.status.startsWith('Red'))
+            .map(cer => cer.message)
+          return {
+            message: custom_message.join(','),
+            nodes: [e.source_node_name, e.target_node_name]
+          }
+        } else {
+          return {
+            message: e.message,
+            nodes: []
+          }
+        }
+      })
+      return {
+        passed: edge_statuses_that_fail.length === 0,
+        support_messages: edge_statuses_that_fail
+      }
     }
   }
 });
