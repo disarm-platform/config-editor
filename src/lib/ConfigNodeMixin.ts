9c6ac7dc11da9c6155d2608683a6809b39892b06
Jonathan Smith <jonathan@peoplesized.com>
2018-06-23 17:57:21 +0200

    type the mixin

--- src/lib/ConfigNodeMixin.ts
+++ src/lib/ConfigNodeMixin.ts
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 import {get} from 'lodash';
+import {TCustomEdgeResponse} from "@locational/config-validation/build/main/lib/TCustomEdgeResponse";
+import {TUnifiedResponse} from "@locational/config-validation/build/module/lib/TUnifiedResponse";
+import {TStandardEdgeResponse} from "@locational/config-validation/build/module/lib/TStandardEdgeResponse";
 
 export default Vue.extend({
   props: {
     config: Object,
     node_name: String,
-    validation_result: Object,
+    validation_result: Object as () => TUnifiedResponse,
   },
   data() {
     return {
@@ -19,13 +22,10 @@ export default Vue.extend({
     }
   },
   computed: {
-    relevant_messages(): any[] {
-      if (this.validation_result.length === 0) {
-        return [];
-      }
-      return this.validation_result.edge_messages.filter(message => {
+    relevant_messages(): TStandardEdgeResponse[] {
+      return this.validation_result.edge_messages.filter((message) => {
         return message.source_node_name === this.node_name || message.target_node_name === this.node_name;
-      })
-    }
-  }
+      });
+    },
+  },
 });
