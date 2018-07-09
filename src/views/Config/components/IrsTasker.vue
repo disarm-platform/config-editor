a9168c0ae6dcb17d63b78327209696219c8a60c5
Nicolai Davies <Nicolai.d@hotmail.com>
2018-07-09 11:26:40 +0200

    Add common applet component to applets

--- src/views/Config/components/IrsTasker.vue
+++ src/views/Config/components/IrsTasker.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
+    <Applet v-model="node_config" @input="emit_change"/>
     <div>{{node_config}}</div>
   </div>
 </template>
 
 <script lang="ts">
   import ConfigNodeMixin from '../ConfigNodeMixin';
+  import Applet from './Applet.vue'
 
   export default ConfigNodeMixin.extend({
+    components: {Applet},
+    methods: {
+      emit_change() {
+        this.$emit('change')
+      }
+    }
   });
 </script>
 
