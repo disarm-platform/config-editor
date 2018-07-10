a0493f46fa09e5af25de91591a18770345d54848
Nicolai Davies <Nicolai.d@hotmail.com>
2018-07-10 09:11:28 +0200

    Can login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,9 @@
   <div>
     
     <div v-if="!user">
-      <el-form  label-width="120px">
+      <el-alert v-if="error.length" :title="error"></el-alert>
+
+      <el-form  label-width="120px" @submit="login">
         <el-form-item label="Username">
           <el-input v-model="username"></el-input>
         </el-form-item>
@@ -18,7 +20,11 @@
     </div>
 
     <div v-else>
-      <el-button type="primary" @click="logout">logout</el-button>
+      <p>Name: {{user.name}}</p>
+      <p>Username: {{user.username}}</p>
+      <p>Instance Slug: {{user.instance_slug}}</p>
+
+      <el-button type="primary" @click="logout">Logout</el-button>
     </div>
 
   </div>
@@ -26,10 +32,13 @@
 
 <script lang='ts'>
   import Vue from 'vue'
+  import { login } from '../lib/auth'
+  import { set_api_key } from '@/lib/standard_handler';
 
   export default Vue.extend({
     data() {
       return {
+        error: '',
         username: '',
         password: ''
       }
@@ -40,8 +49,15 @@
       }
     },
     methods: {
-      login() {
-        this.$store.commit('set_user', {username: this.username, password: this.password})
+      async login() {
+        this.error = ''
+        try {
+          const user = await login(this.username, this.password)
+          set_api_key(user.key)
+          this.$store.commit('set_user', user)
+        } catch (e) {
+          this.error = e.message
+        }
       },
       logout() {
         this.$store.commit('set_user', null)
