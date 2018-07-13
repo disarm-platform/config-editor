cc6699bab6391c45af1148389bb2d6570c70a41a
Jonathan Smith <jonathan@peoplesized.com>
2018-07-13 13:47:05 +0200

    start converting to TS

--- src/Editor.vue
+++ src/Editor.vue
@@ -55,143 +55,97 @@
   </el-tabs>
 </template>
 
-<script>
-import { set, unset } from 'lodash';
-import Vue from 'vue';
-import download from 'downloadjs'
+<script lang="ts">
+  import {cloneDeep, set, unset} from 'lodash';
+  import Vue from 'vue';
+  import download from 'downloadjs';
 
-import Geodata from './views/Geodata.vue';
-import Config from './views/Config/Config.vue';
-import Publish from './views/Publish.vue';
-import Login from './views/Login.vue';
-import Instances from './views/Instances.vue';
+  import Geodata from './views/Geodata.vue';
+  import Config from './views/Config/Config.vue';
+  import Publish from './views/Publish.vue';
+  import Login from './views/Login.vue';
+  import Instances from './views/Instances.vue';
 
-import { get_configuration, create_configuration } from './lib/config';
+  import {get_configuration} from './lib/config';
+  import {TGeodataLayer} from '@locational/geodata-support/build/module/config_types/TGeodata'
 
-import config from './horrible_seed_data/small_valid_config.json';
-
-export default {
-  components: {
-    Login,
-    Instances,
-    Config,
-    Geodata,
-    Publish,
-  },
-  data() {
-    return {
-      active_tab: 'instances',
-      config_valid: false,
-      geodata_layers: [],
-      location_selection: null,
-    };
-  },
-  watch: {
-    async selected_config(selected_config) {
-      if (!selected_config) {
-        return;
-      }
+  export default Vue.extend({
+    components: {Login, Instances, Config, Geodata, Publish,}, data() {
+      return {active_tab: 'instances', config_valid: false, geodata_layers: [], location_selection: null,};
+    }, watch: {
+      async selected_config(selected_config) {
+        if (!selected_config) {
+          return;
+        }
+        if (this.creating_new_config) {
+          return;
+        }
+        const config = await get_configuration(selected_config.id);
+        this.$store.commit('set_config', config);
+      },
+    }, computed: {
+      config() {
+        return this.$store.state.config;
+      }, selected_config() {
+        return this.$store.state.instance;
+      }, creating_new_config() {
+        return this.$store.state.creating_new_config;
+      }, validation_result() {
+        return this.$store.state.validation_result;
+      }, geodata_errors() {
+        const node_name = 'geodata';
+        return this.$store.state.validation_result.errors.filter((response) => {
+          return response.source_node_name === node_name || response.target_node_name === node_name;
+        });
+      },
+    }, async mounted() {
       if (this.creating_new_config) {
         return;
       }
-
-      const config = await get_configuration(selected_config.id);
+      if (!this.selected_config) {
+        return;
+      }
+      const config = await get_configuration(this.selected_config.id);
       this.$store.commit('set_config', config);
+    }, methods: {
+      change(updated_config, pathname, included) {/* save config to store here*/
+        if (included) {/* Need to use lodash.set so nested objects get updated If not we end up with an object like: { 'applets.irs_record_point': {} } when we want: { 'applets': {'irs_record_point: {}} }*/
+          const new_config = {...this.config};
+          set(new_config, pathname, updated_config);
+          this.$store.commit('set_config', new_config);
+        } else {/* use unset for same reason as above*/
+          const new_config = {...this.config};
+          unset(new_config, pathname);
+          this.$store.commit('set_config', new_config);
+        }
+      }, async save_config() {
+        const config_copy = cloneDeep(this.config);
+        /* bump version number TODO: remove string and number conversions*/
+        const current_version = Number(config_copy.config_version);
+        config_copy.config_version = `${current_version + 1}`;
+        /* update, so user can see change*/
+        this.$store.commit('set_config', config_copy);
+        /* send to remote*/
+        /* update local list / reload local lost*/
+        this.$refs.instances.get_list_of_configurations();
+        /* remove _id as we want to create a new version*/
+        delete config_copy._id;
+        try {/* await create_configuration(config_copy);*/
+          download(JSON.stringify(config_copy), `${config_copy.config_id}.config.json`, 'text/plain');
+          this.$store.commit('set_creating_new_config', false);
+        } catch (e) {
+          console.log('e', e);
+        }
+      }, set_geodata_layers(geodata_layers: TGeodataLayer[]) {
+        this.geodata_layers = geodata_layers;
+      },
+      set_location_selection(location_selection: any) {
+        this.location_selection = location_selection;
+      },
     },
-  },
-  computed: {
-    config() {
-      return this.$store.state.config;
-    },
-    selected_config() {
-      return this.$store.state.instance;
-    },
-    creating_new_config() {
-      return this.$store.state.creating_new_config;
-    },
-    validation_result() {
-      return this.$store.state.validation_result
-    },
-    geodata_errors() {
-      const node_name = 'geodata'
-      return this.$store.state.validation_result.errors.filter((response) => {
-        return response.source_node_name === node_name || response.target_node_name === node_name;
-      });
-    }
-  },
-  async mounted() {
-    if (this.creating_new_config) {
-      return;
-    }
-    if (!this.selected_config) {
-      return;
-    }
-
-    const config = await get_configuration(this.selected_config.id);
-    this.$store.commit('set_config', config);
-  },
-  methods: {
-    change(updated_config, pathname, included) {
-      // save config to store here
-
-      if (included) {
-        // Need to use lodash.set so nested objects get updated
-        // If not we end up with an object like:
-        // {
-        //   'applets.irs_record_point': {}
-        // }
-        // when we want:
-        // {
-        //   'applets': {'irs_record_point: {}}
-        // }
-        const new_config = { ...this.config };
-        set(new_config, pathname, updated_config);
-
-        this.$store.commit('set_config', new_config);
-      } else {
-        // use unset for same reason as above
-        const new_config = { ...this.config };
-        unset(new_config, pathname);
-
-        this.$store.commit('set_config', new_config);
-      }
-    },
-    async save_config() {
-      const config_copy = { ...this.config };
-
-      // bump version number
-      // TODO: remove string and number conversions
-      const current_version = Number(config_copy.config_version);
-      config_copy.config_version = `${current_version + 1}`;
-
-      // update, so user can see change
-      this.$store.commit('set_config', config_copy);
-
-      // send to remote
-
-      // update local list / reload local lost
-      this.$refs.instances.get_list_of_configurations();
-
-      // remove _id as we want to create a new version
-      delete config_copy._id;
-
-      try {
-        // await create_configuration(config_copy);
-        download(JSON.stringify(config_copy), `${config_copy.config_id}.config.json`, 'text/plain');
-        this.$store.commit('set_creating_new_config', false);
-      } catch (e) {
-        console.log('e', e);
-      }
-    },
-    set_geodata_layers(geodata_layers) {
-      this.geodata_layers = geodata_layers;
-    },
-    set_location_selection(location_selection) {
-      this.location_selection = location_selection;
-    }
-  },
-};
+  });
 </script>
+
 <style>
   .red {
     color: red;
