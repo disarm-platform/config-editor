d96c2f03e078b98d5f10710bdbfb0d6881fa0161
Nicolai Davies <Nicolai.d@hotmail.com>
2018-07-16 12:15:48 +0200

    Simple component

---
+++ src/pages/Config/JSONEditor.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <el-input rows="20" type="textarea" v-model="local_node_config"></el-input>
+    <el-button>Format</el-button>
+    <el-button>Save</el-button>
+    <div>{{node_config}}</div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  props: ['node_config'],
+  data() {
+    return {
+      error_passing_json: false,
+      local_node_config: '',
+    };
+  },
+  watch: {
+    node_config: {
+      handler() {
+        console.log(true);
+      },
+      deep: true,
+    },
+  },
+  created() {
+    this.set_local_node_config();
+  },
+  methods: {
+    set_local_node_config() {
+      try {
+        this.error_passing_json = false;
+        this.local_node_config = JSON.stringify(this.node_config, undefined, 4);
+      } catch (e) {
+        // passing the json failed
+        this.error_passing_json = true;
+      }
+    },
+  },
+});
+</script>
--- src/pages/Config/components/Aggregations.vue
+++ src/pages/Config/components/Aggregations.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <el-input rows="20" type="textarea" :value="node_config_string" @input="set_node_config_from_string"></el-input>
+    <JSONEditor :node_config="node_config"/>
     <div>{{node_config}}</div>
   </div>
 </template>
 
 <script lang="ts">
   import ConfigNodeMixin from '../ConfigNodeMixin';
+  import JSONEditor from '../JSONEditor.vue';
 
   export default ConfigNodeMixin.extend({
+    components: {JSONEditor},
   });
 </script>
 
