0c2db82a16a302618053e5b418066281d43b077a
Nicolai Davies <Nicolai.d@hotmail.com>
2018-08-16 10:03:31 +0200

    Fix type error

--- src/helpers/upload_file_as_text.ts
+++ src/helpers/upload_file_as_text.ts
@@ -2,7 +2,7 @@ export function upload_file_as_text(file: File): Promise<string> {
   return new Promise((resolve) => {
     const reader = new FileReader();
     reader.onloadend = () => {
-      resolve(reader.result);
+      resolve(reader.result as string);
     };
     reader.readAsText(file);
   });
