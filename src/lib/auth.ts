61a1248f8cbdf5afc8f7eb6243a671bdbf325ddd
mosaic141688 <mbhamalimosaic@gmail.com>
2018-08-24 09:53:16 +0200

    Login path

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,7 @@ import { standard_handler } from '../lib/standard_handler';
 
 export async function login(username: string, password: string): Promise<DoumaUser> {
   const options = {
-    url: `/login`,
+    url: `/auth/login`,
     method: 'post',
     data: {
       username,
