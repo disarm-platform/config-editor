971d2352e240ded0566b40c76f3eb299e871e770
mosaic141688 <mbhamalimosaic@gmail.com>
2018-09-26 16:15:26 +0200

    Create user form

--- src/pages/Users/NewUser.vue
+++ src/pages/Users/NewUser.vue
@@ -1,14 +1,40 @@
 <template>
     <div>
-        New User
+        <el-button type="text" @click="dialogFormVisible = true">Add User</el-button>
+
+        <el-dialog title="New User Details" :visible.sync="dialogFormVisible">
+            <el-form :model="form">
+                <el-form-item label="Name" :label-width="formLabelWidth">
+                     <el-input v-model="form.name" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="Username" :label-width="formLabelWidth">
+                    <el-input v-model="form.name" autocomplete="off"></el-input>
+                </el-form-item>
+                 <el-form-item label="Password" :label-width="formLabelWidth">
+                    <el-input v-model="form.name" autocomplete="off"></el-input>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogFormVisible = false">Cancel</el-button>
+                <el-button type="primary" @click="dialogFormVisible = false">Save</el-button>
+            </span>
+        </el-dialog>
     </div>
 </template>
 <script  lang="ts">
-import Vue from 'vue';
-export default  Vue.extend({
+import Vue from "vue";
+export default Vue.extend({
   name: "users",
   data() {
-      return {}
+    return {
+      dialogFormVisible: false,
+      form: {
+        name: "",
+        username: "",
+        password: ""
+      },
+      formLabelWidth: "120px"
+    };
   }
 });
 </script>
